54d67eee6b717b8b1cbc0ccd4130091e74b27f0d
hantang <34468720+hantang@users.noreply.github.com>
2024-09-12 00:23:02 +0800

    Add scripts etc.

---
+++ docs/list.md
@@ -0,0 +1,17 @@
+# RIME List
+
+[![Awesome](https://awesome.re/badge.svg)](https://github.com/hantang/rime-list)
+![GitHub Commit Badge](https://img.shields.io/github/last-commit/hantang/rime-list.svg)
+
+## 说明
+
+最近更新: <!-- START-DATE -->*2024-09-XX*<!-- END-DATE -->
+
+备注：
+
+- 🎋 表示fork的仓库
+- 🗃️ 表示归档或长久不更新的仓库
+
+<!-- START-TABLE -->
+
+<!-- END-TABLE -->
---
+++ run-site.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -eu
+
+echo "Sync submodule"
+git submodule update --init --recursive --depth=1
+
+# echo "Update docs"
+# if [[ -d docs/wiki ]]; then
+#     rm -rf docs/wiki
+# fi
+# mkdir -p docs/wiki
+# cp -r rime-wiki/*.md docs/wiki/
+
+if [[ -f rime-list/README.md ]]; then
+    echo "Copy rime-list"
+    cp rime-list/README.md docs/list.md
+fi
+
+echo "Build sites"
+mkdocs build >/dev/null 2>&1
+
+echo Done
