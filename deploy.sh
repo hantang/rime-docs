b161d68c33ec5c9307cf269f1a89cc824b421864
hantang <34468720+hantang@users.noreply.github.com>
2025-04-16 15:41:06 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt >/dev/null 2>&1
 
 if [[ -f .gitmodules ]]; then
     echo "Sync submodule"
-    git submodule update --depth=1 --init  --remote --recursive rime-list
+    git submodule update --depth=1 --init --remote --recursive rime-list
 fi
 
 # echo "Update docs"
@@ -16,9 +16,11 @@ fi
 # mkdir -p docs/wiki
 # cp -r rime-wiki/*.md docs/wiki/
 
-if [[ -f rime-list/README.md ]]; then
+readme_file="rime-list/README.md"
+if [[ -f $readme_file ]]; then
     echo "Copy rime-list"
-    cp rime-list/README.md docs/list.md
+    # cp rime-list/README.md docs/list.md
+    sed -E 's/(!\[[^]]*\]\([^)]+\))/\1{ loading=lazy }/g' $readme_file >docs/list.md
 fi
 
 echo "Build sites"
